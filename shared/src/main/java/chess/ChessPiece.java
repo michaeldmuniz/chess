e298491b9d290e2e943c6d9064a4c593d4817a3b
Mike Muniz <mikemuniz@Mikes-MacBook-Air-2.local>
2025-09-15 06:09:20 -0600

    Bishop moves are showing the same but the test isn't passing

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,7 +53,44 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
+    public List<ChessMove> getBishopMoves(ChessPosition start) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        int startRow = start.getRow();
+        int startCol = start.getColumn();
+
+        // Four diagonal directions
+        int[][] directions = {
+                {1,  1},  // up-right
+                {1, -1},  // up-left
+                {-1, 1},  // down-right
+                {-1,-1}   // down-left
+        };
+
+        for (int[] dir : directions) {
+            int row = startRow + dir[0];
+            int col = startCol + dir[1];
+
+            // Keep going in this direction until we leave the board
+            while (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
+                moves.add(new ChessMove(start, new ChessPosition(row, col), null));
+
+                row += dir[0]; // move further in same direction
+                col += dir[1];
+            }
+        }
+
+        return moves;
+    }
+
+
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if(piece.getPieceType() == PieceType.BISHOP){
+            return getBishopMoves(myPosition);
+        }
         return List.of();
     }
 }
