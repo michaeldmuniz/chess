9b8111030e9057e5e62978c65f665852757052bc
Mike Muniz <mikemuniz@mac.lan>
2025-10-16 19:58:29 -0600

    Added ClearHandler.java

---
+++ server/src/main/java/server/ClearHandler.java
@@ -0,0 +1,38 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.ClearService;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+// Handles HTTP DELETE requests to /db.
+
+public class ClearHandler implements Handler {
+
+    private final ClearService service;
+    private final Gson gson = new Gson();
+
+    public ClearHandler(ClearService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            service.clear();
+            ctx.status(200);
+            ctx.result(gson.toJson(new Object())); // returns {}
+        } catch (DataAccessException e) {
+            ctx.status(500);
+            ctx.result(gson.toJson(
+                    Map.of("message", "Error: " + e.getMessage())
+            ));
+        } catch (Exception e) {
+            ctx.status(500);
+            ctx.result(gson.toJson(
+                    Map.of("message", "Error: unexpected failure")
+            ));
+        }
+    }
+}
