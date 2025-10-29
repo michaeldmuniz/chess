8eb5e3f31f4de02f9d9c45963e5f45265b93bd19
Mike Muniz <mikemuniz@mac.lan>
2025-10-28 21:39:07 -0600

    Made 3 tables using SQL and created MySQLDaataAccess file

---
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -0,0 +1,71 @@
+package dataaccess;
+
+import model.*;
+import chess.ChessGame;
+import com.google.gson.Gson;
+
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.List;
+
+public class MySQLDataAccess implements DataAccess {
+
+    private final Gson gson = new Gson();
+
+    public MySQLDataAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private void configureDatabase() throws DataAccessException {
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public int createGame(GameData game) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        throw new DataAccessException("not implemented");
+    }
+}
