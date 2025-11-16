786c9e722520eddb8dc3aabf46e2acfc031fd3f0
Mike Muniz <mikemuniz@mac.lan>
2025-11-16 13:58:19 -0700

    Updated main.java

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,6 +1,7 @@
 package client;
 
-import client.ui.PreloginUI;
+import client.ui.*;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,16 +9,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String serverUrl = "http://localhost:8080";
-        ServerFacade server = new ServerFacade(serverUrl);
+        String url = "http://localhost:8080";
+        ServerFacade server = new ServerFacade(url);
 
         PreloginUI prelogin = new PreloginUI(server, scanner);
-
-        System.out.println("👑 Welcome to 240 chess. Type Help to get started. 👑");
+        PostloginUI postlogin = new PostloginUI(server, scanner);
 
         boolean quit = false;
+        boolean loggedIn = false;
+        String authToken = null;
+
+        System.out.println("👑 Welcome to 240 chess. Type help to begin.");
+
         while (!quit) {
-            quit = prelogin.runOnce();
+            if (!loggedIn) {
+                PreloginResult res = prelogin.runOnce();
+                if (res.quit) quit = true;
+                if (res.loggedIn) {
+                    loggedIn = true;
+                    authToken = res.authToken;
+                }
+            } else {
+                PostloginResult res = postlogin.runOnce(authToken);
+                if (res.quit) quit = true;
+                if (res.loggedOut) {
+                    loggedIn = false;
+                    authToken = null;
+                }
+            }
         }
 
         System.out.println("Goodbye!");
